e523b5a0fe27bd44112001654ef7dfe1a069bc02
Deconto <deconto100@gmail.com>
2020-06-22 20:46:30 -0300

    Update BatchJob5.sh

--- sequencial/BatchJob5.sh
+++ sequencial/BatchJob5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -m abe
 #PBS -V
-#PBS -l nodes=grad03:ppn=1:cluster-Grad,walltime=02:00:00
+#PBS -l nodes=grad01:ppn=1:cluster-Grad,walltime=02:00:00
 #PBS -M bernardo.cesaro@acad.pucrs.br
 #PBS -r n
 #PBS -j oe
