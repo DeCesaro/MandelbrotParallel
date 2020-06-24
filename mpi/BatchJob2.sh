f4b1a7f14e80dba940929e319ee52e1e037e3a50
Deconto <deconto100@gmail.com>
2020-06-24 13:31:32 -0300

    Update BatchJob2.sh

--- mpi/BatchJob2.sh
+++ mpi/BatchJob2.sh
@@ -20,108 +20,108 @@ echo JOB_ID:
 echo 'echo $PBS_JOBID'
 echo #################
 echo "-----------NPOINTS-500-2----------------"
-mpirun -np 2 ./atividade500
+mpirun --bynode -np 2 ./atividade500
 echo "-----------NPOINTS-1000-2--------------"
-mpirun -np 2 ./atividade1000
+mpirun --bynode -np 2 ./atividade1000
 echo "-----------NPOINTS-1500-2--------------"
-mpirun -np 2 ./atividade1500
+mpirun --bynode -np 2 ./atividade1500
 echo "-----------NPOINTS-2000-2--------------"
-mpirun -np 2 ./atividade2000
+mpirun --bynode -np 2 ./atividade2000
 echo "-----------NPOINTS-2500-2--------------"
-mpirun -np 2 ./atividade2500
+mpirun --bynode -np 2 ./atividade2500
 echo "-----------NPOINTS-3000-2--------------"
-mpirun -np 2 ./atividade3000
+mpirun --bynode -np 2 ./atividade3000
 echo "-----------NPOINTS-3500-2--------------"
-mpirun -np 2 ./atividade3500
+mpirun --bynode -np 2 ./atividade3500
 echo "-----------NPOINTS-4000-2--------------"
-mpirun -np 2 ./atividade4000
+mpirun --bynode -np 2 ./atividade4000
 echo "-----------NPOINTS-4500-2--------------"
-mpirun -np 2 ./atividade4500
+mpirun --bynode -np 2 ./atividade4500
 echo "-----------NPOINTS-5000-2--------------"
-mpirun -np 2 ./atividade5000
+mpirun --bynode -np 2 ./atividade5000
 echo "-------------------------------------------------"
 echo "-----------NPOINTS-500-4----------------"
-mpirun -np 4 ./atividade500
+mpirun --bynode -np 4 ./atividade500
 echo "-----------NPOINTS-1000-4--------------"
-mpirun -np 4 ./atividade1000
+mpirun --bynode -np 4 ./atividade1000
 echo "-----------NPOINTS-1500-4--------------"
-mpirun -np 4 ./atividade1500
+mpirun --bynode -np 4 ./atividade1500
 echo "-----------NPOINTS-2000-4--------------"
-mpirun -np 4 ./atividade2000
+mpirun --bynode -np 4 ./atividade2000
 echo "-----------NPOINTS-2500-4--------------"
-mpirun -np 4 ./atividade2500
+mpirun --bynode -np 4 ./atividade2500
 echo "-----------NPOINTS-3000-4--------------"
-mpirun -np 4 ./atividade3000
+mpirun --bynode -np 4 ./atividade3000
 echo "-----------NPOINTS-3500-4--------------"
-mpirun -np 4 ./atividade3500
+mpirun --bynode -np 4 ./atividade3500
 echo "-----------NPOINTS-4000-4--------------"
-mpirun -np 4 ./atividade4000
+mpirun --bynode -np 4 ./atividade4000
 echo "-----------NPOINTS-4500-4--------------"
-mpirun -np 4 ./atividade4500
+mpirun --bynode -np 4 ./atividade4500
 echo "-----------NPOINTS-5000-4--------------"
-mpirun -np 4 ./atividade5000
+mpirun --bynode -np 4 ./atividade5000
 echo "-------------------------------------------------"
 echo "-----------NPOINTS-500-8----------------"
-mpirun -np 8 ./atividade500
+mpirun --bynode -np 8 ./atividade500
 echo "-----------NPOINTS-1000-8--------------"
-mpirun -np 8 ./atividade1000
+mpirun --bynode -np 8 ./atividade1000
 echo "-----------NPOINTS-1500-8--------------"
-mpirun -np 8 ./atividade1500
+mpirun --bynode -np 8 ./atividade1500
 echo "-----------NPOINTS-2000-8--------------"
-mpirun -np 8 ./atividade2000
+mpirun --bynode -np 8 ./atividade2000
 echo "-----------NPOINTS-2500-8--------------"
-mpirun -np 8 ./atividade2500
+mpirun --bynode -np 8 ./atividade2500
 echo "-----------NPOINTS-3000-8--------------"
-mpirun -np 8 ./atividade3000
+mpirun --bynode -np 8 ./atividade3000
 echo "-----------NPOINTS-3500-8--------------"
-mpirun -np 8 ./atividade3500
+mpirun --bynode -np 8 ./atividade3500
 echo "-----------NPOINTS-4000-8--------------"
-mpirun -np 8 ./atividade4000
+mpirun --bynode -np 8 ./atividade4000
 echo "-----------NPOINTS-4500-8--------------"
-mpirun -np 8 ./atividade4500
+mpirun --bynode -np 8 ./atividade4500
 echo "-----------NPOINTS-5000-8--------------"
-mpirun -np 8 ./atividade5000
+mpirun --bynode -np 8 ./atividade5000
 echo "-------------------------------------------------"
 echo "-----------NPOINTS-500-16--------------"
-mpirun -np 16 ./atividade500
+mpirun --bynode -np 16 ./atividade500
 echo "-----------NPOINTS-1000-16------------"
-mpirun -np 16 ./atividade1000
+mpirun --bynode -np 16 ./atividade1000
 echo "-----------NPOINTS-1500-16------------"
-mpirun -np 16 ./atividade1500
+mpirun --bynode -np 16 ./atividade1500
 echo "-----------NPOINTS-2000-16------------"
-mpirun -np 16 ./atividade2000
+mpirun --bynode -np 16 ./atividade2000
 echo "-----------NPOINTS-2500-16------------"
-mpirun -np 16 ./atividade2500
+mpirun --bynode -np 16 ./atividade2500
 echo "-----------NPOINTS-3000-16------------"
-mpirun -np 16 ./atividade3000
+mpirun --bynode -np 16 ./atividade3000
 echo "-----------NPOINTS-3500-16------------"
-mpirun -np 16 ./atividade3500
+mpirun --bynode -np 16 ./atividade3500
 echo "-----------NPOINTS-4000-16------------"
-mpirun -np 16 ./atividade4000
+mpirun --bynode -np 16 ./atividade4000
 echo "-----------NPOINTS-4500-16------------"
-mpirun -np 16 ./atividade4500
+mpirun --bynode -np 16 ./atividade4500
 echo "-----------NPOINTS-5000-16------------"
-mpirun -np 16 ./atividade5000
+mpirun --bynode -np 16 ./atividade5000
 echo "-------------------------------------------------"
 echo "-----------NPOINTS-500-32--------------"
-mpirun -np 32 ./atividade500
+mpirun --bynode -np 32 ./atividade500
 echo "-----------NPOINTS-1000-32------------"
-mpirun -np 32 ./atividade1000
+mpirun --bynode -np 32 ./atividade1000
 echo "-----------NPOINTS-1500-32------------"
-mpirun -np 32 ./atividade1500
+mpirun --bynode -np 32 ./atividade1500
 echo "-----------NPOINTS-2000-32------------"
-mpirun -np 32 ./atividade2000
+mpirun --bynode -np 32 ./atividade2000
 echo "-----------NPOINTS-2500-32------------"
-mpirun -np 32 ./atividade2500
+mpirun --bynode -np 32 ./atividade2500
 echo "-----------NPOINTS-3000-32------------"
-mpirun -np 32 ./atividade3000
+mpirun --bynode -np 32 ./atividade3000
 echo "-----------NPOINTS-3500-32------------"
-mpirun -np 32 ./atividade3500
+mpirun --bynode -np 32 ./atividade3500
 echo "-----------NPOINTS-4000-32------------"
-mpirun -np 32 ./atividade4000
+mpirun --bynode -np 32 ./atividade4000
 echo "-----------NPOINTS-4500-32------------"
-mpirun -np 32 ./atividade4500
+mpirun --bynode -np 32 ./atividade4500
 echo "-----------NPOINTS-5000-32------------"
-mpirun -np 32 ./atividade5000
+mpirun --bynode -np 32 ./atividade5000
 echo "-------------------------------------------------"
 echo Final Time is 'date'
