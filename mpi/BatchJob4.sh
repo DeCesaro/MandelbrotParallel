e2587cfe4b3c545826978134a43a4779ce0f1edb
GuilhermeDeconto <deconto100@gmail.com>
2020-06-24 15:04:53 -0300

    update

--- mpi/BatchJob4.sh
+++ mpi/BatchJob4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -m abe
 #PBS -V
-#PBS -l nodes=4:ppn=16:cluster-Grad,walltime=02:00:00
+#PBS -l nodes=4:ppn=16:cluster-Grad,walltime=01:00:00
 #PBS -M guilherme.deconto@acad.pucrs.br
 #PBS -r n
 #PBS -j oe
